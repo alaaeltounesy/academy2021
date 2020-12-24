8b5539c711278181631c0d290baef28e2ddcd572
Emad <elbembo@users.noreply.github.com>
2020-12-24 18:19:53 +0200

    fh

--- header-links.php
+++ header-links.php
@@ -1,11 +1,10 @@
 <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <meta name="Description" content="Enter your description here" />
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.2/css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.0/css/all.min.css">
-    <link rel="stylesheet" href="/themes/<?php echo sitedata('site_theme') ?>/assets/css/owl.carousel.min.css" />
-    <link rel="stylesheet" href="/themes/<?php echo sitedata('site_theme') ?>/assets/css/owl.theme.default.min.css">
-    <link rel="stylesheet" href="/themes/<?php echo sitedata('site_theme') ?>/assets/css/circle.css">
-  
-    <link rel="stylesheet" href="/themes/<?php echo sitedata('site_theme') ?>/assets/css/style.min.css">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta http-equiv="X-UA-Compatible" content="ie=edge">
+<meta name="Description" content="Enter your description here" />
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.2/css/bootstrap.min.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.0/css/all.min.css">
+<link rel="stylesheet" href="/themes/<?php echo sitedata('site_theme') ?>/assets/css/owl.carousel.min.css" />
+<link rel="stylesheet" href="/themes/<?php echo sitedata('site_theme') ?>/assets/css/owl.theme.default.min.css">
+<link rel="stylesheet" href="/themes/<?php echo sitedata('site_theme') ?>/assets/css/circle.css">
+<link rel="stylesheet" href="/themes/<?php echo sitedata('site_theme') ?>/assets/css/style.min.css">
